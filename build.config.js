fd06122d26ba988046fb6a87b268fb56ccf52a8a
wltsmrz <wltsmrz@gmail.com>
2014-09-03 17:30:32 -0700

    Bump ripple-lib version

--- build.config.js
+++ build.config.js
@@ -87,7 +87,7 @@ module.exports = {
       'vendor/jscrollpane/script/jquery.jscrollpane.js',
       'deps/jquery.selectbox.min.js',
       'deps/jquery.inview.js',
-      'deps/ripple-0.7.37.js',
+      'deps/ripple-0.7.39-debug.js',
       'deps/offersExercisedListener.js',
       'deps/onResize.js'
     ],
