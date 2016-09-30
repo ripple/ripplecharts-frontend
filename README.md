eaa3e0c59c1f6e6d1fc4533fda4e7d28a1d393ee
Matthew Fettig <matthew@ripple.com>
2016-09-30 09:38:32 -0700

    make tesSUCCESS green (#140)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![CircleCI](https://circleci.com/gh/ripple/ripplecharts-frontend.svg?style=svg)](https://circleci.com/gh/ripple/ripplecharts-frontend)
+
 # Ripple Charts
 
 This is the frontend for ripplecharts.com data visualization using angular.js and D3
@@ -8,24 +10,24 @@ This is the frontend for ripplecharts.com data visualization using angular.js an
 2. Install [bower](http://bower.io/)
 
         $ sudo npm install -g bower
-        
+
 2. Install [Grunt](http://gruntjs.com/).
 
         $ sudo npm install -g grunt-cli
-    
+
 3. Install the Ripple Charts frontend.
-    
+
         $ git clone https://github.com/ripple/ripplecharts-frontend
         $ cd ripplecharts-frontend
         $ npm install
         $ bower install
-        
+
 4. Copy 'deployment.environments.json.example' into 'deployment.environments.json'
 5. Copy 'src/example.config.js' into 'src/config.js' and fill in the options as desired. A URL for the api is required.
 
 ## Build
 
-1. Run the following command to build the client.    
+1. Run the following command to build the client.
 
         $ grunt
 
--- src/common/transactionStats.js
+++ src/common/transactionStats.js
@@ -1,18 +1,53 @@
-var TransactionStats = function (options) {
+/* eslint {no-unused-vars: 0} */
+/* global StackedChart */
+'use strict';
+
+function TransactionStats(options) {
   var self = this;
+  var colorFunction;
 
   if (options.id) {
-    self.div = d3.select('#'+options.id).attr('class','chartWrap');
+    self.div = d3.select('#' + options.id).attr('class', 'chartWrap');
   } else {
-    self.div = d3.select('body').append('div').attr('class','chartWrap');
+    self.div = d3.select('body').append('div').attr('class', 'chartWrap');
   }
 
+  if (options.metric === 'result') {
+    colorFunction = (function() {
+      var color = d3.scale.linear()
+      .domain([0, 10])
+      .interpolate(d3.interpolateHcl)
+      .range([d3.rgb('#660000'), d3.rgb('#884400')]);
 
-//create new line chart
+      var domain = [];
+
+      function c(d) {
+        if (d === 'tesSUCCESS') {
+          return 'green';
+        }
+
+        return color(domain.indexOf(d));
+      }
+
+      c.domain = function(d) {
+        if (d) {
+          domain = d;
+          return this;
+        }
+
+        return domain;
+      };
+
+      return c;
+    })();
+  }
+
+  // create new line chart
   var chart = new StackedChart({
     div: self.div,
     title: '# of Transactions',
-    resize: true
+    resize: true,
+    color: colorFunction
   });
 
   var start = moment.utc().subtract(999, 'week');
